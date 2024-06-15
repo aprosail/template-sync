6df54add6adc7b3acb6df305c5b483ca0c9ac5da
James Aprosail <aprosail@outlook.com>
2024-06-15 11:09:23 +0800

    rename simplify test fn

--- src/options.rs
+++ src/options.rs
@@ -65,7 +65,7 @@ mod tests {
     use super::{FileOrFolder, FileSyncMode, Options};
 
     #[test]
-    fn test_json() {
+    fn json() {
         let raw = r#"
         {
             "requireGitignore": true,
